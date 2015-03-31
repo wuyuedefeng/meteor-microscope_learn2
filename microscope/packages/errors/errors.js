934ea8ff6e396a8c0f9a95bcee56abce22daf0e8
wangsen <wangs@tongxinyiliao.com>
2015-03-31 15:35:59 +0800

    创建提交评论表单

--- microscope/packages/errors/errors.js
+++
@@ -1,9 +0,0 @@
-// Write your package code here!
-ItrydoErrors = {
-    // Local (client-only) collection
-    collection: new Mongo.Collection(null),
-
-    throw: function(message) {
-        ItrydoErrors.collection.insert({message: message, seen: false})
-    }
-};
